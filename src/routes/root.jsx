be3410d18de66e800e5b0d2e0db52273e87b5029
bailong97 <palmermatthew645@gmail.com>
2023-08-10 23:29:28 -0600

    Add delete functionality and edit functionality.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,16 @@
-import { Outlet, Link, useLoaderData, Form, } from "react-router-dom";
-import { getContacts, createContact } from "../contacts"
+import {
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  redirect,
+  useNavigation,
+} from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
 
 export async function action() {
-    const contact = await createContact();
-    return { contact};
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export async function loader() {
@@ -13,6 +20,8 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
+
   return (
     <>
       <div id="sidebar">
@@ -31,14 +40,19 @@ export default function Root() {
           </form>
           <Form method="post">
             <button type="submit">New</button>
-            </Form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -47,7 +61,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -57,9 +71,11 @@ export default function Root() {
             </p>
           )}
         </nav>
-
       </div>
-      <div id="detail">
+      <div
+        id="detail"
+        className={navigation.state === "loading" ? "loading" : ""}
+      >
         <Outlet />
       </div>
     </>
