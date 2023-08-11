d5c2e2a71cf07a9d01946698c6b6c7c5f81a478d
bailong97 <palmermatthew645@gmail.com>
2023-08-10 22:40:15 -0600

    Add rounter for Contact and EditContact

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,32 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Contact, { loader as contactLoader, } from "./routes/contact";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-]);
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      loader: rootLoader,
+      action: rootAction,
+      children: [
+        {
+          path: "contacts/:contactId",
+          element: <Contact />,
+          loader: contactLoader,
+        },
+        {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+        },
+      ],
+    },
+  ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
